fd60ace974e3d6b42653516ec16804bd40972c95
bhavya2728 <55987588+bhavya2728@users.noreply.github.com>
2019-10-02 14:25:37 +0530

    Add files via upload

---
+++ assig.2-15.cpp
@@ -0,0 +1,12 @@
+#include<fstream.h>
+
+int main()
+{
+	ofstream fout;
+	fout.open("out.txt");
+	char str[300]="Time is a great teacher but unfortunately it kills
+	 all its pupils. Berlioz";
+	fout<<str;
+	fout.close();
+	return 0;
+}
